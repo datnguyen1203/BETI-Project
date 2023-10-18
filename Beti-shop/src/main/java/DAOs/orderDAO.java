ccc1c2e3f59da4a56edd7664e305c9e851492d85
atuankhuu <khuuanhtuan123@gmail.com>
2023-10-19 00:27:36 +0700

    Lam xong dashboard Manager

--- Beti-shop/src/main/java/DAOs/orderDAO.java
+++ Beti-shop/src/main/java/DAOs/orderDAO.java
@@ -4,10 +4,16 @@
  */
 package DAOs;
 
+import Models.Order;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,19 +28,69 @@ public class orderDAO {
     public orderDAO() {
         conn = DBContext.DBContext.getConnection();
     }
-    
+
     //addOrder
-    public void addOrder(){
+    public void addOrder() {
         LocalDate curDate = LocalDate.now();
         String date = curDate.toString();
-        try{
+        try {
             //Add Order
             String sql = "insert into [order] values (?,?,?,?)";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setInt(1, 0);
+        } catch (Exception e) {
+
         }
-        catch (Exception e){
-            
+    }
+
+    //getall
+    public List<Order> getAllOrder() {
+        List<Order> list = new ArrayList<>();
+        String sql = "select * from [order]";
+        try {
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Order(rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getInt(3),
+                        rs.getInt(4),
+                        rs.getFloat(5),
+                        rs.getDate(6)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(orderDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return list;
+    }
+    //Delete Order
+    public int DeleteOrder(int id) {
+        int kq = 0;
+        String sql = "delete from [Order] where order_id=?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            kq = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(orderDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return kq;
+    }
+    //get Order
+    public int getOrderByAC(int id) {
+        rs = null;
+        int kq = 0;
+        String sql = "select order_id from [order] where account_id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                kq = rs.getInt("order_id");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(orderDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return kq;
     }
 }
